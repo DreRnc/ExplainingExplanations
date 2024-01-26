3fe74ecc4999f0502159a096ea90b7fca4d04ed4
Andrea Roncoli <115221482+DreRnc@users.noreply.github.com>
2024-01-26 18:15:31 +0100

    Not much

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,29 @@
+def generate_batch_prompts_mnli(batch):
+    """Generate the prompt for MNLI.
+
+    Args:
+        batch (dict of lists): a dictionary containing the lists of premises and hypotheses of length batch_size.
+
+    Returns:
+        list of str: the prompts.
+    """
+    prompts = []
+    for i in range(len(batch['premise'])):
+        prompt = "mnli hypothesis: " + batch['hypothesis'][i] + ' premise: ' + batch['premise'][i]
+        prompts.append(prompt)
+    return prompts
+
 def generate_prompt_mnli(datapoint):
-    return "mnli hypothesis: " + datapoint['hypothesis'] + ' premise: ' + datapoint['premise']
+    """Generate the prompt for MNLI.
+
+    Args:
+        datapoint (dict): a dictionary containing the premise and hypothesis.
+
+    Returns:
+        str: the prompt.
+    """
+    prompt = "mnli hypothesis: " + datapoint['hypothesis'] + ' premise: ' + datapoint['premise']
+    return prompt
 
 def evaluate_output_mnli(output, label):
     """T5 outputs a string. We need to compare it to the label which is
@@ -14,3 +38,9 @@ def evaluate_output_mnli(output, label):
     else:
         raise ValueError('Output not recognized')
     return output == label
+
+def tokenize_function(examples, tokenizer):
+    """Tokenize the dataset. This function is passed to the map method.
+    """
+    prompts = generate_batch_prompts_mnli(examples)
+    return tokenizer(prompts, padding='max_length', truncation=True, max_length=128)
